708a2604a6ea04f5499b464900f8e32746b8dafb
lsivak <larasanders@gmail.com>
2019-02-09 15:19:20 -0500

    separated all js code into js files

--- app/assets/javascripts/candies.js
+++ app/assets/javascripts/candies.js
@@ -14,3 +14,21 @@ function getComments() {
   })
 }
 getComments()
+
+//candies index
+function postCandies() {
+  $.getJSON("/candies/", function(candies) {
+    candies.forEach(candy => {   
+    let candyHTML = "<h3><a href='/candies/" + candy.id + "'>" + candy.name + "</a></h3><br>";
+      $.getJSON("/comments/", function(comments) {
+        comments.forEach(c => {
+          if(c.candy_id == candy.id) {
+            candyHTML += `<li> ${c.name} </li>`
+          }    
+        })
+      $("#" + candy.taste + "Candies").append(candyHTML)
+      });
+    });
+  });
+}
+postCandies();
--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -9,3 +9,32 @@ Comment.prototype.getComments = () => {
   return this.name + this.user_id
 }
   
+function postComments() {
+  $.getJSON("/candies/", function(candies) {
+    candies.forEach(candy => {   
+    let candyHTML = `<h4>${candy.name}</h4><br>`;
+      $.getJSON("/comments/", function(comments) {
+        comments.forEach(c => {
+          if(c.candy_id == candy.id) {
+            candyHTML += `<li><a href='/comments/ ${c.id}'> ${c.name}</a></li>`
+          }    
+        })
+      $(".Comment").append(candyHTML)
+      });
+    });
+  });
+}
+postComments();
+
+//comments show
+  $(function () {
+    $(".js-next").on("click", function(e) {
+      e.preventDefault()
+      var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+      $.get("/comments/" + nextId + ".json", function(comment) {  
+        $(".commentCandyName").text("Candy: " + comment.candy.name);
+        $(".commentName").text("Comment: " + comment.name);
+        $(".js-next").attr("data-id", comment.id);
+      });
+    });
+  });
