fd87529ada989f87e58b7104587e71d264bafb6d
lsivak <larasanders@gmail.com>
2019-02-09 14:03:28 -0500

    fixing my comments index page, moving JS code to JS pages

--- app/assets/javascripts/candies.js
+++ app/assets/javascripts/candies.js
@@ -1,13 +1,10 @@
 //candies show
-function getFormatted() {
-  return this.description + this.user_id
-}
 
 function getComments() {
   $.get("/comments/", function(comments) {  
       var can = parseInt($(".js-next-candy").attr("data-id")) 
         comments.forEach(comment => {
-          Object.getPrototypeOf(comment).getFormatted = () => {
+          comment.getFormatted = () => {
             return comment.name;
           };
           if(comment.candy_id == can) {
--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,9 +1,11 @@
 class Comment {	
-  constructor({description, user_id, candy_id}) {
-    this.description = description	    
+  constructor({name, user_id, candy_id}) {
+    this.name = name    
     this.user_id = user_id
     this.candy_id = candy_id
   };
 }
-
+Comment.prototype.getComments = () => {
+  return this.name + this.user_id
+}
   
